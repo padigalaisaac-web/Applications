6d023a908172b87ff1094c0ef6c4f1c7ce842b02
padigalaisaac-web <padigalaisaac@gmail.com>
2026-08-20 00:08:51 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
-
 import mongoose from 'mongoose';
 
 export async function connectDB(uri) {
+  if (!uri) {
+    throw new Error('MONGODB_URI is not defined');
+  }
+
   mongoose.set('strictQuery', true);
+
   await mongoose.connect(uri);
-  console.log('MongoDB connected');
+
+  console.log('MongoDB connected successfully');
 }
