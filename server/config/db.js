a776a82a86547a3004483c493fb13e2726308145
padigalaisaac-web <padigalaisaac@gmail.com>
2026-08-19 22:02:53 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,8 @@
+
 import mongoose from 'mongoose';
 
-export const connectDB = async (uri) => {
-  try {
-    const conn = await mongoose.connect(uri);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Failed to connect to MongoDB: ${error.message}`);
-    process.exit(1);
-  }
-};
+export async function connectDB(uri) {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(uri);
+  console.log('MongoDB connected');
+}
